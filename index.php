1f25b0dfd6fff301bf1f35bd2876257c262715f5
Igor Tikhonov <igor.tikhonov@texuna.com>
2017-12-08 16:41:24 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require './mysql.php';
+print("www-host: " . $a_host . "<BR><HR>");
+$link = mysqli_connect( $mysql_host , $mysql_user , $mysql_pass , $mysql_db );
+if (!mysqli_set_charset($link, "utf8")) {
+	    printf("Ошибка при загрузке набора символов utf8: %s\n", mysqli_error($link));
+	            exit();}
+
+if ($link == false){
+	    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+} else {
+
+	$sql = 'SELECT * FROM phonelist';
+
+	$result = mysqli_query($link, $sql);
+
+	if ($result == false) {
+
+		        print("Произошла ошибка при выполнении запроса");
+			    }
+	    else {
+
+		             while ($row = mysqli_fetch_array($result)) {
+				         print("N: " . $row['contact_id'] . " Имя: " . $row['first_name'] ." Фамилия: " . $row['last_name'] ." телефон : " . $row['phone'] . "<br>");
+			     }
+	    }
+}
+?>
+<HR>
+
+
+
